915d4e8e51d278895b066e6b1aac68a8a2a169ff
jrfrigat <frostfriman@gmail.com>
2026-06-28 14:15:18 +0300

    Trim IDE Layout page intro to the shell itself

--- src/Flare.Components.IDE/Css/Classes/Toolbar.cs
+++ src/Flare.Components.IDE/Css/Classes/Toolbar.cs
@@ -1,6 +1,6 @@
 namespace Flare.Css.Classes.Ide;
 
-/// <summary><c>FlareToolbar</c> / <c>FlareQuickAccessToolbar</c> command strips.</summary>
+/// <summary><c>FlareToolbar</c> classic icon command strip.</summary>
 public static class Toolbar
 {
     public const string Root = "flare-toolbar";
